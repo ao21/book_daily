8b5c929854822e64af7d17b1c44a49669d68e46d
ao21 <lpubnrm.ay@gmail.com>
2021-04-21 09:27:51 +0900

    初期データにユーザーネームを追加

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,8 +1,10 @@
+NAME = 'test'
 EMAIL = 'test@example.com'
 PASSWORD = 'password'
 
 # テストユーザーが存在しないときだけ作成
 User.find_or_create_by!(email: EMAIL) do |user|
+  user.name = NAME
   user.password = PASSWORD
   puts 'ユーザーの初期データインポートに成功しました。'
 end
