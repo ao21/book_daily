f4fa50b8890a82b469e891ee7350b9116fca74a7
ao21 <lpubnrm.ay@gmail.com>
2021-05-18 12:27:11 +0900

    タスク編集機能を実装

--- app/controllers/tasks_controller.rb
+++ app/controllers/tasks_controller.rb
@@ -25,9 +25,13 @@ class TasksController < ApplicationController
   end
 
   def edit
+    @task = Task.find(params[:id])
   end
 
   def update
+    task = Task.find(params[:id])
+    task.update!(task_params)
+    redirect_to task, notice: "更新しました"
   end
 
   def destroy
