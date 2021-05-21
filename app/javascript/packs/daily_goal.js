ae698a7289489b6be07fe3c32d92cea937d73eb2
ao21 <lpubnrm.ay@gmail.com>
2021-05-21 14:34:31 +0900

    1日あたりのページ数を計算する式を追加

--- app/javascript/packs/daily_goal.js
+++ app/javascript/packs/daily_goal.js
@@ -4,9 +4,12 @@ document.addEventListener("turbolinks:load", function(){
     var finish = $("#finished_on").val();
     var dateStart = Date.parse(start);
     var dateFinish = Date.parse(finish);
+    var pageCount = $("#page").data("page-id")
 
-    var result = (dateFinish - dateStart) / 86400000;
+    var resultDays = (dateFinish - dateStart) / 86400000;
+    var resultPages = Math.ceil( pageCount / resultDays )
 
-    $('.days').html(result);
+    $("#days").html(resultDays);
+    $("#page").html(resultPages)
   });
 });
