1e4cb44ff0d155502ca1e072f87019edca1665a0
ao21 <lpubnrm.ay@gmail.com>
2021-05-26 10:56:32 +0900

    進捗新規登録機能を実装

--- app/controllers/reads_controller.rb
+++ app/controllers/reads_controller.rb
@@ -6,9 +6,19 @@ class ReadsController < ApplicationController
   end
 
   def new
+    @task = Task.find(params[:task_id])
+    @read = Read.new
   end
 
   def create
+    task = Task.find_by(id: params[:task_id])
+    read = task.reads.build(read_params)
+
+    if read.save
+      redirect_to task_path(task.id)
+    else
+      render "tasks/show"
+    end
   end
 
   def edit
@@ -18,5 +28,11 @@ class ReadsController < ApplicationController
   end
 
   def destroy
+  end
+
+  private
+
+  def read_params
+    params.require(:read).permit(:read_on, :read_page)
   end
 end
