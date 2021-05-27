59dfbe2190b0c139cbf11a64108c6182c3beb820
ao21 <lpubnrm.ay@gmail.com>
2021-05-28 08:27:08 +0900

    Readモデルにバリデーションを追加

--- app/models/read.rb
+++ app/models/read.rb
@@ -1,3 +1,7 @@
 class Read < ApplicationRecord
   belongs_to :task
+
+  validates: read_on, presence: true
+  validates: read_page, presence: true, numericality: { only_integer: true }
+  validates: task_id, presence: true
 end
