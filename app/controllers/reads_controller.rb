cd92ae95a2f39b80d92604d567879dd487f849df
ao21 <lpubnrm.ay@gmail.com>
2021-05-27 13:31:52 +0900

    コントローラ処理を before_action で共通化

--- app/controllers/reads_controller.rb
+++ app/controllers/reads_controller.rb
@@ -1,43 +1,42 @@
 class ReadsController < ApplicationController
-  def show
-  end
+  before_action :set_task
+  before_action :set_read, only: %i[edit update destroy]
 
   def new
-    @task = Task.find(params[:task_id])
     @read = Read.new
   end
 
   def create
-    task = Task.find(params[:task_id])
-    read = task.reads.build(read_params)
-
+    read = @task.reads.build(read_params)
     if read.save
-      redirect_to task_path(task.id)
+      redirect_to task_path(@task.id)
     else
       render "tasks/show"
     end
   end
 
   def edit
-    @task = Task.find(params[:task_id])
-    @read = @task.reads.find(params[:id])
   end
 
   def update
-    task = Task.find(params[:task_id])
-    read = task.reads.find(params[:id])
-    read.update!(read_params)
-    redirect_to task_path(task.id)
+    @read.update!(read_params)
+    redirect_to task_path(@task.id)
   end
 
   def destroy
-    task = Task.find(params[:task_id])
-    read = task.reads.find(params[:id])
-    read.destroy!
-    redirect_to task_path(task.id)
+    @read.destroy!
+    redirect_to task_path(@task.id)
   end
 
   private
+
+  def set_task
+    @task = Task.find(params[:task_id])
+  end
+
+  def set_read
+    @read = @task.reads.find(params[:id])
+  end
 
   def read_params
     params.require(:read).permit(:read_on, :read_page)
