d5fb48da172364696277c322b7fa9b62b3fcbe6b
ao21 <lpubnrm.ay@gmail.com>
2021-06-14 20:53:30 +0900

    タスク一覧の枠を設定

--- app/javascript/packs/toggle_menu.js
+++ app/javascript/packs/toggle_menu.js
@@ -1,10 +1,8 @@
 document.addEventListener('turbolinks:load', function () {
-  $(function () {
-    $('.js-menu-item-link').each(function () {
-      $(this).on('click', function () {
-        $('+.submenu', this).slideToggle();
-        return false;
-      });
+  $('.js-menu-item-link').each(function () {
+    $(this).on('click', function () {
+      $('+.submenu', this).slideToggle();
+      return false;
     });
   });
 });
