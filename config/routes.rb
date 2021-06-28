4746689c71863c3be968055c87f9d0be8bdf3da6
ao21 <lpubnrm.ay@gmail.com>
2021-06-28 15:33:56 +0900

    ルーティングを編集

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,12 @@
 Rails.application.routes.draw do
-  devise_for :users
+  devise_for :users, controllers: {
+    registrations: 'users/registrations'
+  }
   root "home#index"
+
+  devise_scope :user do
+    post 'users/guest_sign_in', to: 'users/sessions#guest_sign_in'
+  end
 
   resources :books, only: [:new, :create]
   get '/books', to: 'books#search'
