fef667a5d782a3eb8cf7bca0c06696fef48f711c
ao21 <lpubnrm.ay@gmail.com>
2021-07-23 19:55:03 +0900

    ヘッダーのリンクをタスク詳細ページへのリンクに変更

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,5 +1,6 @@
 class ApplicationController < ActionController::Base
   before_action :configure_permitted_parameters, if: :devise_controller?
+  before_action :first_task
 
   private
 
@@ -11,5 +12,10 @@ class ApplicationController < ActionController::Base
 
   def after_sign_in_path_for(resorce)
     tasks_path
+  end
+
+  def first_task
+    task = Task.joins(:reads).order(read_on: :desc).limit(1)
+    @first_task = task[0]
   end
 end
