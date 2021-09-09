ac7554aa8c72080217ef46ac6ac178c27bc34bb0
ao21 <lpubnrm.ay@gmail.com>
2021-09-09 09:36:28 +0900

    HeaderMenu__content を領域外をクリックで閉じる様に js ファイルを更新

--- app/javascript/packs/js-header_menu.js
+++ app/javascript/packs/js-header_menu.js
@@ -1,7 +1,12 @@
 document.addEventListener('turbolinks:load', function () {
-  $('.js-HeaderMenu__toggle').each(function () {
-    $(this).on('click', function () {
-      $('.js-HeaderMenu__toggle-icon, .js-HeaderMenu__content').toggleClass('show');
-    });
+  $('.js-HeaderMenu__toggle').on('click', function () {
+    $('.js-HeaderMenu__toggle-icon, .js-HeaderMenu__content').toggleClass('show');
+    return false;
+  });
+
+  $(document).on('click touchend', function (event) {
+    if (!$(event.target).closest('.js-HeaderMenu__content').length) {
+      $('.js-HeaderMenu__toggle-icon, .js-HeaderMenu__content').removeClass('show');
+    }
   });
 });
