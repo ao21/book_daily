1f209a0d2c525f4febe5f71bf295b11d30b1758b
ao21 <lpubnrm.ay@gmail.com>
2021-09-26 11:19:19 +0900

    Userモデルのテストの表記を微修正

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -1,36 +1,28 @@
 require 'rails_helper'
 
 RSpec.describe User, type: :model do
-  it "has a valid factory" do
-    expect(FactoryBot.build(:user)).to be_valid
-  end
+  describe "バリデーション" do
+    it "FactoryBotのデータがバリデーションを通ること" do
+      expect(FactoryBot.build(:user)).to be_valid
+    end
 
-  it "is valid with name, email, password and password_confirmation" do
-    user = User.new(
-      name: "Aren",
-      email: "tester@example.com",
-      password: "apple-strawberry-grape-orange",
-      password_confirmation: "apple-strawberry-grape-orange"
-    )
-    expect(user).to be_valid
-  end
+    it "is invalid without a name" do
+      user = FactoryBot.build(:user, name: nil)
+      user.valid?
+      expect(user.errors[:name]).to include("を入力してください")
+    end
 
-  it "is invalid without a name" do
-    user = FactoryBot.build(:user, name: nil)
-    user.valid?
-    expect(user.errors[:name]).to include("を入力してください")
-  end
+    it "is invalid without an email address" do
+      user = FactoryBot.build(:user, email: nil)
+      user.valid?
+      expect(user.errors[:email]).to include("を入力してください")
+    end
 
-  it "is invalid without an email address" do
-    user = FactoryBot.build(:user, email: nil)
-    user.valid?
-    expect(user.errors[:email]).to include("を入力してください")
-  end
-
-  it "is invalid with a diplicate email adress" do
-    FactoryBot.create(:user, email: "aaron@example.com")
-    user = FactoryBot.build(:user, email: "aaron@example.com")
-    user.valid?
-    expect(user.errors[:email]).to include("はすでに存在します")
+    it "is invalid with a diplicate email adress" do
+      FactoryBot.create(:user, email: "aaron@example.com")
+      user = FactoryBot.build(:user, email: "aaron@example.com")
+      user.valid?
+      expect(user.errors[:email]).to include("はすでに存在します")
+    end
   end
 end
