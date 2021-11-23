e757791d28cdca527132528e578e873500ff7499
ao21 <lpubnrm.ay@gmail.com>
2021-11-23 15:00:28 +0900

    readsコントローラのcreateアクションを修正

--- app/controllers/reads_controller.rb
+++ app/controllers/reads_controller.rb
@@ -3,15 +3,19 @@ class ReadsController < ApplicationController
   before_action :set_read, only: %i[edit update destroy]
 
   def new
-    @read = Read.new
+    if params
+      @read = Read.new(read_on: params[:read_on])
+    else
+      @read = Read.new
+    end
   end
 
   def create
     @read = @task.reads.build(read_params)
     if @read.save
-      redirect_to task_path(@task.id), notice: "登録しました"
+      redirect_to @read, notice: "進捗を登録しました。"
     else
-      flash.now[:alert] = "登録に失敗しました"
+      flash.now[:alert] = "進捗の登録に失敗しました。"
       render :new
     end
   end
