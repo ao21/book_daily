7fa870b14786cfacba342a73884eb9dbf0c80318
ao21 <lpubnrm.ay@gmail.com>
2021-12-06 10:15:57 +0900

    [add]ヘッダーに検索フォームを追加

--- app/javascript/packs/search_form.js
+++ app/javascript/packs/search_form.js
@@ -1,6 +1,6 @@
 document.addEventListener('turbolinks:load', function () {
   $(function () {
-    var search_form = $('#search_form');
+    var search_form = $('#js-SearchForm');
     search_form.submit(function () {
       var keyword = $('input[id=keyword]').val();
       if (keyword.match(/^[ 　\r\n\t]*$/)) {
