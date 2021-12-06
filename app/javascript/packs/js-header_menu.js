b8405af8e1841f5ac852718f53b98d06300b7744
ao21 <lpubnrm.ay@gmail.com>
2021-12-06 10:14:55 +0900

    [add]ハンバーガーメニューを作成

--- app/javascript/packs/js-header_menu.js
+++ app/javascript/packs/js-header_menu.js
@@ -5,8 +5,9 @@ document.addEventListener('turbolinks:load', function () {
   });
 
   $(document).on('click touchend', function (event) {
-    if (!$(event.target).closest('#js-Header__content, #js-Header__toggle').length) {
-      $('#js-Header__toggle, #js-Header__content').removeClass('show');
+    if (!$(event.target).closest('#js-Header__list, #js-Header__toggle').length) {
+      $('#js-Header__toggle, #js-Header__content, #js-Header__list').removeClass('show');
+      return false;
     }
   });
 });
