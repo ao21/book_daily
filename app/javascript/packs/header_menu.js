81ba6afb3e51c371d21a6df14283835e6bd4e97f
ao21 <lpubnrm.ay@gmail.com>
2021-12-13 19:17:51 +0900

    [fix]sp幅のデザイン調整

--- app/javascript/packs/header_menu.js
+++ app/javascript/packs/header_menu.js
@@ -4,12 +4,10 @@ document.addEventListener('turbolinks:load', function () {
     return false;
   });
 
-  if ($('#js-Header__toggle, #js-Header__content, #js-Header__list').hasClass('show')) {
-    $(document).on('click touchend', function (event) {
-      if (!$(event.target).closest('#js-Header__list, #js-Header__toggle').length) {
-        $('#js-Header__toggle, #js-Header__content, #js-Header__list').removeClass('show');
-        return false;
-      }
-    });
-  }
+  $(document).on('click touchend', function (event) {
+    if (!$(event.target).closest('#js-Header__list, #js-Header__toggle').length) {
+      $('#js-Header__toggle, #js-Header__content, #js-Header__list').removeClass('show');
+      return false;
+    }
+  });
 });
