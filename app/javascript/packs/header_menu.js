517f2d94d08adc29722464420715aafb7ce8acab
ao21 <lpubnrm.ay@gmail.com>
2021-12-15 19:56:33 +0900

    [fix]ロゴのリンクを阻害しないよう修正

--- app/javascript/packs/header_menu.js
+++ app/javascript/packs/header_menu.js
@@ -5,9 +5,13 @@ document.addEventListener('turbolinks:load', function () {
   });
 
   $(document).on('click touchend', function (event) {
-    if (!$(event.target).closest('#js-Header__list, #js-Header__toggle').length) {
-      $('#js-Header__toggle, #js-Header__content, #js-Header__list').removeClass('show');
-      return false;
+    if (!$('#js-Header__toggle').hasClass('show')) {
+      return;
+    } else {
+      if (!$(event.target).closest('#js-Header__list, #js-Header__toggle').length) {
+        $('#js-Header__toggle, #js-Header__content, #js-Header__list').removeClass('show');
+        return false;
+      }
     }
   });
 });
