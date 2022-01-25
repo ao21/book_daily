a5f6f51e3140b603b20e971fb54e3f9ac784d61d
ao21 <lpubnrm.ay@gmail.com>
2022-01-25 09:01:00 +0900

    [add]コメントを追加

--- app/lib/google_books_api.rb
+++ app/lib/google_books_api.rb
@@ -1,11 +1,13 @@
 module GoogleBooksApi
 
-  def get_json_from_url(url)
-    JSON.parse(Net::HTTP.get(URI.parse(Addressable::URI.encode(url))))
-  end
-
+  # キーワード検索
   def url_from_keyword(keyword)
     limit = 20
     "https://www.googleapis.com/books/v1/volumes?q=#{keyword}&country=JP&maxResults=#{limit}"
+  end
+
+  # 検索結果のJSONを取得
+  def get_json_from_url(url)
+    JSON.parse(Net::HTTP.get(URI.parse(Addressable::URI.encode(url))))
   end
 end
